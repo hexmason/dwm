28cf7b633ffc40ff3fbb8cd32e70aeb336e59151
hexmason <hexmason@proton.me>
2025-08-27 13:31:13 +0300

    Replaced standard dmenu command with custom wrapper in exitdwm.c

--- exitdwm.c
+++ exitdwm.c
@@ -22,7 +22,7 @@ void exitdwm ()
 # endif
 
 # define S_LOCK "Lock"
-# define S_RESTART_DWM "restart Dwm"
+# define S_RESTART_DWM "Restart dwm"
 # define S_OFFSCREEN "Off-screen"
 # define S_EXIT "Exit"
 # define S_REBOOT "Reboot"
@@ -40,12 +40,11 @@ void exitdwm ()
 	FILE * exit_menu = popen (
 		"echo \""
 			S_FORMAT (LOCK) "\n"
-			S_FORMAT (RESTART_DWM) "\n"
 			S_FORMAT (OFFSCREEN) "\n"
 			S_FORMAT (EXIT) "\n"
 			S_FORMAT (REBOOT) "\n"
 			S_FORMAT (SHUTDOWN)
-			"\" | dmenu -p exit: | " S_FORMAT_CLEAR
+			"\" | dmenu-wal -p exit: | " S_FORMAT_CLEAR
 		,
 		"r"
 	);
